d54c01094d3a35bfdee9573c8699af6c3e402a7c
Nick_Sun <yarronicksun@gmail.com>
2023-03-03 21:34:08 +0500

    Init

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -1,1 +1,47 @@
 from aiogram import Router
+from aiogram.filters import Command, CommandStart, Text
+from aiogram.types import Message
+from keyboards.keyboards import yes_no_kb, game_kb
+from lexicon.lexicon import LEXICON_RU
+from servises.servises import get_bot_choice, get_winner
+
+
+router: Router = Router()
+
+# Хендлер команды /start
+@router.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer(text=LEXICON_RU['/start'], reply_markup=yes_no_kb)
+
+# Хендлер команды /help
+@router.message(Command(commands=['/help']))
+async def process_help_command(message: Message):
+    await message.answer(text=LEXICON_RU['/help'], reply_markup=yes_no_kb)
+
+# Хендлер на согласие пользователя играть в игру
+@router.message(Text(text=LEXICON_RU['yes_button']))
+async def process_yes_answer(message: Message):
+    await message.answer(text=LEXICON_RU['yes'], reply_markup=game_kb)
+
+# Хендлер на отказ пользователя играть
+@router.message(Text(text=LEXICON_RU['no_button']))
+async def process_no_answer(message: Message):
+    await message.answer(text=LEXICON_RU['no'])
+
+# Хендлер срабатывает на любую из игровых кнопок
+@router.message(Text(text=[LEXICON_RU['rock'],
+                           LEXICON_RU['paper'],
+                           LEXICON_RU['scissors']]))
+async def process_game_button(message: Message):
+    bot_choice = get_bot_choice()
+    await message.answer(text=f'{LEXICON_RU["bot_choice"]} '
+                              f'- {LEXICON_RU[bot_choice]}')
+    winner = get_winner(message.text, bot_choice)
+    await message.answer(text=LEXICON_RU[winner], reply_markup=yes_no_kb)
+
+
+
+
+
+
+
