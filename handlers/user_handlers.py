d4edab7285826082d9af830ee56ef7f3e2a2cc0e
Nick_Sun <yarronicksun@gmail.com>
2023-03-04 12:02:52 +0500

    Init

--- handlers/user_handlers.py
+++ handlers/user_handlers.py
@@ -5,28 +5,32 @@ from keyboards.keyboards import yes_no_kb, game_kb
 from lexicon.lexicon_ru import LEXICON_RU
 from servises.servises import get_bot_choice, get_winner
 
-
 router: Router = Router()
+
 
 # Хендлер команды /start
 @router.message(CommandStart())
 async def process_start_command(message: Message):
     await message.answer(text=LEXICON_RU['/start'], reply_markup=yes_no_kb)
 
+
 # Хендлер команды /help
 @router.message(Command(commands=['/help']))
 async def process_help_command(message: Message):
     await message.answer(text=LEXICON_RU['/help'], reply_markup=yes_no_kb)
+
 
 # Хендлер на согласие пользователя играть в игру
 @router.message(Text(text=LEXICON_RU['yes_button']))
 async def process_yes_answer(message: Message):
     await message.answer(text=LEXICON_RU['yes'], reply_markup=game_kb)
 
+
 # Хендлер на отказ пользователя играть
 @router.message(Text(text=LEXICON_RU['no_button']))
 async def process_no_answer(message: Message):
     await message.answer(text=LEXICON_RU['no'])
+
 
 # Хендлер срабатывает на любую из игровых кнопок
 @router.message(Text(text=[LEXICON_RU['rock'],
@@ -38,10 +42,3 @@ async def process_game_button(message: Message):
                               f'- {LEXICON_RU[bot_choice]}')
     winner = get_winner(message.text, bot_choice)
     await message.answer(text=LEXICON_RU[winner], reply_markup=yes_no_kb)
-
-
-
-
-
-
-
